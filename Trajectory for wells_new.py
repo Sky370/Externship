dfa8634e5d84a43e4c09c180877a4260ecb60105
North Campus <elh1873@utulsa.edu>
2022-06-16 18:12:05 -0500

    Update

--- Trajectory for wells_new.py
+++ Trajectory for wells_new.py
@@ -8,7 +8,7 @@ import os
 # warnings.filterwarnings("ignore")
 
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-my_file = os.path.join(THIS_FOLDER, 'Directional Survey Data.xlsx')
+my_file = os.path.join(THIS_FOLDER, 'Directional Survey Data_6.15.2022.xlsx')
 
 new = pd.read_excel(my_file)
 tvd = new["TVDCalc"].tolist()
@@ -19,5 +19,11 @@ fig = px.line_3d(new, x="NSCalc", y="EWCalc", z=[-x for x in tvd], color="WELL N
     "z":"TVD",
     "WELL NAME":"WELL NAMES"
     })
+
+fig1 = px.line(new, x= "NSCalc", y="DLSCalc", labels={
+    "NSCalc":"North-South",
+    "DLSCalc":"DLS"
+})
 fig.update_traces(line_width=2.5)
 fig.show()
+fig1.show()
